6eb4204d2627671322f38f082eb93788cc0b92e4
Syed Saifullah Tarique <s.saifullahtarique@gmail.com>
2019-02-23 12:14:17 +0530

    commit

--- src/com/codingblocks/Lecture_21/AdjMapGraphWeighted.java
+++ src/com/codingblocks/Lecture_21/AdjMapGraphWeighted.java
@@ -167,6 +167,87 @@ public class AdjMapGraphWeighted<T> {
         return true;
     }
 
+    //criskal method
+    public int mst(){
+        ArrayList<Edge> edges = new ArrayList<>();
+
+        for (Vertex vertex: vertexHashMap.values()) {
+            for (Vertex padoci:vertex.neighbours.values()) {
+                int weight = vertex.weights.get(padoci.value);
+                Edge e = new Edge(vertex,padoci,weight);
+            }
+        }
+        Collections.sort(edges);
+
+        int mst_weight = 0;
+        Map<Vertex, Vertex> parents = generateParents();
+
+        for (Edge edge: edges) {
+            if(union(edge.first,edge.second,parents)){
+                mst_weight += edge.weight;
+            }
+        }
+        return mst_weight;
+    }
+
+    public AdjMapGraphWeighted<T> mstGraph(){
+        AdjMapGraphWeighted<T> graph = new AdjMapGraphWeighted<>();
+        for (T value: vertexHashMap.keySet()) {
+            graph.addVertex(value);
+        }
+        ArrayList<Edge> edges = new ArrayList<>();
+
+        for (Vertex vertex : vertexHashMap.values()) {
+            for (Vertex padoci : vertex.neighbours.values()) {
+                int weight = vertex.weights.get(padoci.value);
+                Edge e = new Edge(vertex,padoci,weight);
+            }
+        }
+
+        Collections.sort(edges);
+
+        Map<Vertex, Vertex> parents = generateParents();
+
+        for (Edge edge: edges) {
+            if(union(edge.first,edge.second,parents)){
+                graph.addEdge(edge.first.value,edge.second.value,edge.weight);
+            }
+        }
+        return graph;
+    }
+
+    //prims method of mst
+    public int prims(){
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        Set<Vertex> visited = new HashSet<>();
+
+        //first item into s_vertex
+        Vertex s_vertex = vertexHashMap.values().iterator().next();
+        visited.add(s_vertex);
+
+        for (Vertex padosi: s_vertex.neighbours.values()) {
+            Edge edge = new Edge(s_vertex, padosi,s_vertex.weights.get(padosi.value));
+            queue.add(edge);
+        }
+        int mst = 0;
+        while (!queue.isEmpty()){
+            Edge front = queue.remove();
+            if(visited.contains(front.first) && visited.contains(front.second)){
+                continue;
+            }
+
+            mst += front.weight;
+            Vertex unvisited = null;
+            unvisited = visited.contains(front.first) ? front.second : front.first;
+
+            for (Vertex padosi: unvisited.neighbours.values()) {
+                Edge edge = new Edge(unvisited, padosi, unvisited.weights.get(padosi.value));
+                queue.add(edge);
+            }
+        }
+        return mst;
+    }
+
     private class Vertex{
         private T value;
         private HashMap<T, Vertex> neighbours;
@@ -184,7 +265,7 @@ public class AdjMapGraphWeighted<T> {
         }
     }
 
-    private class Edge{
+    private class Edge implements Comparable<Edge>{
         private Vertex first;
         private Vertex second;
         private int weight;
@@ -193,6 +274,11 @@ public class AdjMapGraphWeighted<T> {
             this.first = first;
             this.second = second;
             this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.weight - o.weight;
         }
     }
 }
